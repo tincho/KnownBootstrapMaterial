134722b7065c121b5ccad951894a6a081aa47fb5
tincho <martin.salinas@gmail.com>
2018-12-16 22:17:31 -0300

    lightbox enhancements

--- templates/default/lightbox/shell/footer.tpl.php
+++ templates/default/lightbox/shell/footer.tpl.php
@@ -4,7 +4,21 @@
 $(document).on('click', '[data-toggle="lightbox"][data-remote]', function(event) {
 	event.preventDefault();
 	event.stopPropagation();
-	var $link = $(this);
-	$link.ekkoLightbox();
+	var $extLink = $('<a target="blank" class="badge badge-light ml-2" href=""><i class="fa fa-external-link-alt"></i></a>');
+	var _timeout;
+	$(this).ekkoLightbox({
+		onShown: function() {
+			clearTimeout(_timeout);
+			// clicking on video controls may trigger the navigation . 
+			// _timeout = setTimeout(this._$modalArrows.show.bind(this._$modalArrows), 1000);
+		},
+		onContentLoaded: function() {
+			var showLink = this._$element.attr('href') != this._$element.data('remote');
+			if (showLink) {
+				$extLink.attr('href', this._$element.attr('href'));
+				this._$modalFooter.append($extLink.fadeIn());
+			}
+		}
+	});
 });
 </script>
